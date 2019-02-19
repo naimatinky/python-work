6a5cb08b7472b1c33358d76a8ff6b41cb96e528a
pixeydust18 <pixeydust18@gmail.com>
2019-02-18 20:37:48 -0800

    make changes

--- vowels.py
+++ vowels.py
@@ -1,22 +1,27 @@
-from collections import OrderedDict
-def vowels(word):
-    vowel = ['a','e','i','o','u']
-    list_word = list(word)
-    new_list = [char for char in list_word if char in vowel]
-    tuple1 =tuple(new_list)
-    remove_duplicates="".join(OrderedDict.fromkeys(tuple1))
-    get_unique_vowels_tuple =(remove_duplicates,)
-    get_dup1_char=[ letter for letter in word.count(letter)>1]
-    get_unique_char="".join(OrderedDict.fromkeys(get_dup1_char))
-    tuple_dup=tuple(get_unique_char)
-    count_tuple_dup=len(tuple_dup)
-    count_tuple_dup_final=(count_tuple_dup,)
-    combine_tuple=get_unique_vowels_tuple + count_tuple_dup_final
-    print(combine_tuple)
-    
-word="viviandoreen"
-    
-vowels(word)
+def my_Vowels(stringInput):
+    vowels = ['a', 'e', 'i', 'o', 'u']
+    listFoundVowels = []
+    StringChars = set()
+    DuplicatedChars = set()
+
+    try:
+        stringInput = stringInput.replace(' ', '')
+
+        for each in stringInput:
+            if (each in vowels and each not in listFoundVowels):
+                listFoundVowels.append(each)
+            if(each in StringChars):
+                DuplicatedChars.add(each)
+            else:
+                StringChars.add(each)
+
+            stringvoweltuple = ''.join(tuple(listFoundVowels))
+        return (stringvoweltuple, len(DuplicatedChars))
+    except TypeError:
+        return("Invalid input")
+
+print(my_Vowels('dahdah'))
+print(my_Vowels('drink water')) 
                               
 
     
